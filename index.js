95850e89eeaa7facc37a9ec4c15c7080cf289e32
GermainMichaud <gmichaud.dev@gmail.com>
2021-07-18 12:23:26 +0200

    Add Electron (dev-dep) & initailize electron app

---
+++ index.js
@@ -0,0 +1,26 @@
+const { BrowserWindow, app } = require('electron');
+
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Devtools => debug
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.loadFile('./public/index.html');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
